a23f38e1c5cafca786497442bc247b421b37c472
Marcel Admiraal <madmiraal@users.noreply.github.com>
2022-10-18 17:24:45 +0100

    Create initial Decrypt activity and layout.

---
+++ app/src/main/java/com/myperioddataismine/Decrypt.kt
@@ -0,0 +1,11 @@
+package com.myperioddataismine
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Decrypt : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.decrypt)
+    }
+}
