0da09294b128a02ef08f6c393d9d75c577a7d129
Marcel Admiraal <madmiraal@users.noreply.github.com>
2026-07-13 17:04:37 +0200

    Update strings and icons

--- app/src/main/kotlin/com/myperioddataismine/DayData.kt
+++ app/src/main/kotlin/com/myperioddataismine/DayData.kt
@@ -22,34 +22,74 @@ class DayData {
 
     companion object: Table {
         val flowLevelValues = arrayOf (
-            R.string.none to R.drawable.none,
+            R.string.nothing to R.drawable.nothing,
+            R.string.spotting to R.drawable.spotting,
             R.string.light to R.drawable.drop1,
             R.string.moderate to R.drawable.drop2,
             R.string.heavy to R.drawable.drop3,
-            R.string.omg to R.drawable.drop4
+            R.string.omg to R.drawable.omg
         )
 
         val moodValues = arrayOf(
             R.string.happy to R.drawable.happy,
+            R.string.energised to R.drawable.energised,
+            R.string.relaxed to R.drawable.relaxed,
             R.string.sad to R.drawable.sad,
-            R.string.tired to R.drawable.tired,
-            R.string.sick to R.drawable.sick,
-            R.string.angry to R.drawable.angry
+            R.string.unmotivated to R.drawable.unmotivated,
+            R.string.pessimistic to R.drawable.pessimistic,
+            R.string.irritable to R.drawable.irritable,
+            R.string.angry to R.drawable.angry,
+            R.string.anxious to R.drawable.anxious,
+            R.string.stressed to R.drawable.stressed
         )
 
-        val symptomValues = arrayOf(
+        val painValues = arrayOf(
             R.string.abdominal_pain to R.drawable.abdominal_pain,
-            R.string.abdominal_swelling to R.drawable.abdominal_swelling,
             R.string.backache to R.drawable.backache,
-            R.string.fatigue to R.drawable.fatigue,
+            R.string.cramps to R.drawable.cramps_placeholder,
             R.string.headache to R.drawable.headache,
-            R.string.hip_pain to R.drawable.hip_pain,
-            R.string.irritability to R.drawable.irritability,
-            R.string.migraine_pain to R.drawable.migraine_pain,
+            R.string.joint_pain to R.drawable.joint_pain,
+            R.string.migraine to R.drawable.migraine,
             R.string.muscle_pain to R.drawable.muscle_pain,
+            R.string.ovulation_pain to R.drawable.ovulation_pain_placeholder,
+            R.string.tender_breasts to R.drawable.tender_breasts_placeholder
+        )
+        val symptomValues = arrayOf(
+            R.string.acne to R.drawable.acne_placeholder,
+            R.string.bloated to R.drawable.bloated,
+            R.string.burning_mouth to R.drawable.burning_mouth_placeholder,
+            R.string.constipation to R.drawable.constipation_placeholder,
+            R.string.cravings to R.drawable.cravings_placeholder,
+            R.string.diarrhoea to R.drawable.diarrhoea_placeholder,
+            R.string.discharge to R.drawable.discharge_placeholder,
+            R.string.dizziness to R.drawable.dizziness_placeholder,
+            R.string.fatigued to R.drawable.fatigued,
+            R.string.hot_flashes to R.drawable.hot_flashes_placeholder,
+            R.string.insomnia to R.drawable.insomnia_placeholder,
+            R.string.itchiness to R.drawable.itchiness_placeholder,
+            R.string.nausea to R.drawable.nausea_placeholder,
             R.string.stomach_upset to R.drawable.stomach_upset,
-            R.string.sweat to R.drawable.sweat,
-            R.string.vomit to R.drawable.vomit
+            R.string.sweating to R.drawable.sweating,
+            R.string.uti to R.drawable.uti_placeholder,
+            R.string.vaginal_dryness to R.drawable.vaginal_dryness_placeholder,
+            R.string.vomiting to R.drawable.vomiting
+        )
+
+        val activityValues = arrayOf(
+            R.string.solo to R.drawable.solo_placeholder,
+            R.string.partner to R.drawable.partner_placeholder,
+            R.string.vaginal to R.drawable.vaginal_placeholder,
+            R.string.oral to R.drawable.oral_placeholder,
+            R.string.anal to R.drawable.anal_placeholder,
+            R.string.toys to R.drawable.toys_placeholder
+        )
+
+        val protectionValues = arrayOf(
+            R.string.condom to R.drawable.condom_placeholder,
+            R.string.diaphragm to R.drawable.diaphragm_placeholder,
+            R.string.sponge to R.drawable.sponge_placeholder,
+            R.string.spermicide to R.drawable.spermicide_placeholder,
+            R.string.emergency_contraception to R.drawable.emergency_contraception_placeholder
         )
 
         // Table Name
